63e09d3a7f5588d32026c311b10ae6c5cba79ddc
ksnaveenkumar2k@gmail.com <KSNaveen@2000>
2025-01-21 13:02:02 +0530

    project setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
